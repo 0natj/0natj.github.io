40e948f33f38c2aa6bdf94822efb75bab610532a
0natj <natalie@Natty-2.local>
2014-12-01 21:14:06 +1300

    add flick and change some looks

---
+++ flick.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $('.flicker-example').flickerplate(
+    {
+        arrows: true,
+        arrows_constraint: false,
+        auto_flick: true,
+        auto_flick_delay: 4,
+        block_text: true,
+        dot_alignment: 'center',
+        dot_navigation: true,
+        flick_animation: 'transition-slide',
+        flick_position: 1,
+        theme: 'light'
+    });
+});
--- nat.js
+++ nat.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    
     $("#home").click(function(){
         window.location = "index.html"
     });
