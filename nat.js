d83b79a265b69821353a3bcd3c6e15e6d72e5b5f
0natj <natalie@Natty.local>
2015-03-07 00:25:06 +1300

    update data

--- nat.js
+++ nat.js
@@ -55,18 +55,36 @@ var Nat = {
         var data = {
             April: [
                 {Date: "Monday the 6th", Time: "1:00pm", Type: "Central League Game", Verses: "Stop Out", Where: "Seatoun Park" },
-                {Date: "Sunday the 12th", Time: "1:00pm", Type: "Central League Game", Verses: "Stop Out", Where: "Seatoun Park" },
-                {Date: "Sunday the 19th", Time: "1:00pm", Type: "Central League Game", Verses: "Stop Out", Where: "Seatoun Park" }
+                {Date: "Sunday the 12th", Time: "1:00pm", Type: "Central League Game", Verses: "Valeron", Where: "Palmerston North" },
+                {Date: "Sunday the 19th", Time: "1:00pm", Type: "Central League Game", Verses: "Moturoa", Where: "Seatoun Park" },
+                {Date: "Sunday the 26th", Time: "1:00pm", Type: "ASB Cup", Verses: "Dunno", Where: "Dunno" }
             ],
             May: [
-                {Date: "Sunday the 6th", Time: "1:00pm", Type: "Central League Game", Verses: "Stop Out", Where: "Seatoun Park" }
+                {Date: "Sunday the 3rd", Time: "1:00pm", Type: "Central League Game", Verses: "BNU", Where: "Seatoun Park" },
+                {Date: "Sunday the 10th", Time: "1:00pm", Type: "ASB Cup", Verses: "Dunno", Where: "Dunno" },
+                {Date: "Sunday the 17th", Time: "1:00pm", Type: "Central League Game", Verses: "Massey", Where: "Palmerston North" },
+                {Date: "Sunday the 24th", Time: "1:00pm", Type: "Central League Game", Verses: "Wellington United", Where: "Newtown Park" },
+                {Date: "Sunday the 31st", Time: "1:00pm", Type: "Central League Game", Verses: "Dunno", Where: "Dunno" }
             ],
-            June: []
+            June: [
+                {Date: "Sunday the 7th", Time: "1:00pm", Type: "Central League Game", Verses: "Upper Hutt", Where: "Seatoun Park" },
+                {Date: "Sunday the 21st", Time: "1:00pm", Type: "Central League Game", Verses: "Dunno", Where: "Dunno" },
+                {Date: "Sunday the 28th", Time: "1:00pm", Type: "Central League Game", Verses: "Valeron", Where: "Seatoun Park" }
+            ],
+            July: [
+                {Date: "Sunday the 5th", Time: "1:00pm", Type: "Central League Game", Verses: "Stop Out", Where: "The Hutt Somewhere" },
+                {Date: "Sunday the 12th", Time: "1:00pm", Type: "Central League Game", Verses: "Moturoa", Where: "New Plymouth" },
+                {Date: "Sunday the 19th", Time: "1:00pm", Type: "Central League Game", Verses: "BNU", Where: "Brooklyn" },
+                {Date: "Sunday the 26th", Time: "1:00pm", Type: "Central League Game", Verses: "Massey", Where: "Seatoun Park" }
+            ],
+            August: [
+                {Date: "Sunday the 9th", Time: "1:00pm", Type: "Central League Game", Verses: "Upper Hutt", Where: "Upper Hutt" },
+                {Date: "Sunday the 15th", Time: "1:00pm", Type: "Central League Game", Verses: "Wellington United", Where: "Seatoun Park" }                 ]
         };
         
         return data;
     },
     getmonths: function () {
-        return ["April", "May", "June"];
+        return ["April", "May", "June", "July", "August"];
     }
 }
